4ee770ed091c068dc48af38903177658faa0d277
Jakub Pieńkowski <8525083+Jakski@users.noreply.github.com>
2022-03-02 07:31:33 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,126 @@
+terraform {
+  backend "swift" {
+    auth_url    = "https://auth.cloud.ovh.net/v3"
+    region_name = "WAW"
+    container   = "terraform-jakski-it"
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {
+  owner = var.organization_name
+  app_auth {}
+}
+
+resource "github_membership" "administrators" {
+  for_each = toset(var.administrators)
+  username = each.key
+  role     = "admin"
+}
+
+resource "github_team" "teams" {
+  for_each    = var.teams
+  name        = each.key
+  description = try(each.value.description, null)
+  privacy     = "closed"
+}
+
+resource "github_team_members" "members" {
+  for_each = var.teams
+  team_id  = github_team.teams[each.key].id
+  dynamic "members" {
+    for_each = try(each.value.members, [])
+    content {
+      username = members.value
+      role     = "member"
+    }
+  }
+}
+
+resource "github_repository" "mirrors" {
+  for_each     = var.mirror_repositories
+  name         = each.key
+  description  = try(each.value.description, null)
+  has_issues   = false
+  has_projects = false
+  has_wiki     = false
+}
+
+resource "github_branch_protection" "mirrors" {
+  for_each            = var.mirror_repositories
+  repository_id       = github_repository.mirrors[each.key].node_id
+  pattern             = "*"
+  enforce_admins      = true
+  push_restrictions   = [var.app_node_id]
+  allows_force_pushes = false
+}
+
+resource "github_repository" "repositories" {
+  for_each    = var.repositories
+  name        = each.key
+  description = try(each.value.description, null)
+  auto_init   = true
+}
+
+resource "github_branch_protection" "repositories" {
+  for_each          = var.repositories
+  repository_id     = github_repository.repositories[each.key].node_id
+  pattern           = "*"
+  push_restrictions = []
+
+  required_status_checks {
+    strict   = true
+    contexts = []
+  }
+
+  required_pull_request_reviews {
+    require_code_owner_reviews      = true
+    required_approving_review_count = 2
+    dismiss_stale_reviews           = true
+    restrict_dismissals             = true
+    dismissal_restrictions          = [
+      github_team.teams["tech-lead"].node_id,
+    ]
+  }
+}
+
+resource "github_team_repository" "teams" {
+  for_each = {for i in flatten([
+    for repository_name, repository in var.repositories : [
+      for team in repository.teams : {
+        repository = repository_name
+        team       = team
+      }
+    ]
+  ]) : "${i.repository}_${i.team}" => i}
+  team_id    = github_team.teams[each.value.team].name
+  repository = github_repository.repositories[each.value.repository].name
+  permission = "push"
+}
+
+resource "github_team_repository" "tech-lead" {
+  for_each   = var.repositories
+  team_id    = github_team.teams["tech-lead"].id
+  repository = github_repository.repositories[each.key].name
+  permission = "maintain"
+}
+
+resource "github_repository_file" "codeowners" {
+  for_each            = var.repositories
+  repository          = github_repository.repositories[each.key].name
+  file                = "CODEOWNERS"
+  content             = "* @${var.organization_name}/tech-lead ${join(
+    " ",
+    formatlist("@${var.organization_name}/%s", each.value.teams)
+  )}"
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Jakski IT Bot"
+  commit_email        = "issues+github@jakski.name"
+  overwrite_on_create = true
+}
---
+++ variables.tf
@@ -0,0 +1,107 @@
+variable "app_node_id" {
+  type    = string
+  default = null
+}
+
+variable "organization_name" {
+  type    = string
+  default = "Jakski-IT"
+}
+
+# NOTE: Administrators are automatically assigned to every team. Trying to
+# assign them will cause phantom changes in provider.
+variable "administrators" {
+  type    = list(string)
+  default = [
+    "Jakski"
+  ]
+}
+
+variable "mirror_repositories" {
+  type    = map
+  default = {
+    sphinxcontrib-autoyaml = {}
+  }
+}
+
+variable "teams" {
+  type = map(object({
+    name        = string
+    description = string
+    members     = list(string)
+  }))
+  default = {
+    tech-lead = {
+      name        = "Tech-lead"
+      description = "Technical leaders"
+      members     = [
+        "JakskiWork",
+      ]
+    }
+
+    infrastructure = {
+      name        = "Infrastructure"
+      description = "Infrastructure mergers"
+      members     = [
+        "JakskiWork",
+      ]
+    }
+
+    content = {
+      name        = "Content"
+      description = "Content mergers"
+      members     = [
+        "JakskiWork",
+      ]
+      repositories = {
+        content1 = {}
+      }
+    }
+
+    distribution = {
+      name        = "Distribution"
+      description = "Distribution mergers"
+      members     = [
+        "JakskiWork",
+      ]
+    }
+  }
+}
+
+# NOTE: Don't assign tech-lead to any repository. They're maintainers by
+# default.
+variable "repositories" {
+  type = map(object({
+    description = string
+    teams = list(string)
+  }))
+
+  default = {
+    terraform = {
+      description = "Terraform files for managing organization infrastructure"
+      teams       = []
+    }
+
+    infra1-test = {
+      description = "Infrastructure repository 1"
+      teams       = [
+        "infrastructure",
+      ]
+    }
+
+    distribution1 = {
+      description = "Distribution 1"
+      teams       = [
+        "distribution"
+      ]
+    }
+
+    shared1 = {
+      description = "Shared repository"
+      teams       = [
+        "content",
+        "distribution",
+      ]
+    }
+  }
+}
