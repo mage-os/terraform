f287ef256aad3312d0d4f28de4548967ff625326
Simon Sprankel <sprankhub@users.noreply.github.com>
2022-04-20 15:51:43 +0200

    Customise variables for mage-os

--- variables.tf
+++ variables.tf
@@ -5,14 +5,12 @@ variable "github_app_pem_file" { default = null }
 variable "app_node_id" { default = null }
 
 variable "organization_name" {
-  default = "Jakski-IT"
+  default = "Mage-OS"
 }
 
 variable "administrators" {
   default = [
-    "Jakski",
-    "Vinai",
-    "sprankhub",
+    "mage-os-ci",
   ]
 }
 
@@ -28,7 +26,7 @@ variable "teams" {
       name        = "Tech-lead"
       description = "Technical leaders"
       members = [
-        "Jakski",
+        "Vinai",
       ]
     }
 
@@ -36,7 +34,8 @@ variable "teams" {
       name        = "Infrastructure"
       description = "Infrastructure mergers"
       members = [
-        "JakskiWork",
+        "Vinai",
+        "sprankhub",
       ]
     }
 
@@ -44,7 +43,9 @@ variable "teams" {
       name        = "Content"
       description = "Content mergers"
       members = [
-        "JakskiWork",
+        "johnhughes1984",
+        "wigman",
+        "Vinai",
       ]
     }
 
@@ -52,9 +53,8 @@ variable "teams" {
       name        = "Distribution"
       description = "Distribution mergers"
       members = [
-        "JakskiWork",
-        "Jakski",
         "Vinai",
+        "sprankhub",
       ]
     }
   }
@@ -63,22 +63,102 @@ variable "teams" {
 variable "repositories" {
   default = {
     terraform = {
-      description = "Terraform files for managing organization infrastructure"
+      description = "Terraform files for managing the organization repository permissions."
     }
 
-    distribution1 = {
-      description = "Distribution 1"
+    infrastructure = {
+      description = "Mage-OS organization infrastructure discussion and GitHub Actions."
+      teams       = ["infrastructure"]
+    }
+
+    mage-os-website = {
+      description = "Source of the mage-os.org website."
+      teams       = ["content"]
+    }
+
+    mageos-magento2 = {
+      description = "Work in progress."
       teams       = ["distribution"]
     }
 
-    shared1 = {
-      description = "Shared repository"
-      teams       = ["content", "distribution"]
+    mirror-adobe-ims = {
+      description = "Mage-OS mirror fork of https://github.com/magento/adobe-ims."
+      teams       = ["continuous-integration"]
     }
 
-    test1 = {
-      description = "Test repository"
-      teams       = ["distribution"]
+    mirror-adobe-stock-integration = {
+      description = "Mage-OS mirror fork of https://github.com/magento/adobe-stock-integration."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-composer-dependency-version-audit-plugin = {
+      description = "Mage-OS mirror fork of https://github.com/magento/composer-dependency-version-audit-plugin."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-composer-root-update-plugin = {
+      description = "Mage-OS mirror fork of https://github.com/magento/composer-root-update-plugin."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-devdocs = {
+      description = "Mage-OS mirror fork of https://github.com/magento/devdocs."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-inventory = {
+      description = "Mage-OS mirror fork of https://github.com/magento/inventory."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-inventory-composer-installer = {
+      description = "Mage-OS mirror fork of https://github.com/magento/inventory-composer-installer."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-magento-composer-installer = {
+      description = "Mage-OS mirror fork of https://github.com/magento/magento-composer-installer."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-magento2 = {
+      description = "Mage-OS mirror fork of https://github.com/magento/magento2."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-magento2-page-builder = {
+      description = "Mage-OS mirror fork of https://github.com/magento/magento2-page-builder."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-magento2-sample-data = {
+      description = "Mage-OS mirror fork of https://github.com/magento/magento2-sample-data."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-module-configurable-sample-data-venia = {
+      description = "Mage-OS mirror fork of https://github.com/magento/module-configurable-sample-data-venia."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-page-builder-types = {
+      description = "Mage-OS mirror fork of https://github.com/magento/page-builder-types."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-security-package = {
+      description = "Mage-OS mirror fork of https://github.com/magento/security-package."
+      teams       = ["continuous-integration"]
+    }
+
+    mirror-zf1 = {
+      description = "Mage-OS mirror fork of https://github.com/magento/zf1."
+      teams       = ["continuous-integration"]
+    }
+
+    namespace = {
+      description = "This repository serves to register the mage-os packagist namespace."
+      teams       = ["infrastructure"]
     }
   }
 }
