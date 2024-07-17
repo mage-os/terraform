47b92529debad14c3ad87ded27039e1f837d55da
Ryan Hoerr <rhoerr@users.noreply.github.com>
2024-07-16 20:17:34 -0400

    chore: grant push_allowances to codeowners for each standard repository

--- main.tf
+++ main.tf
@@ -144,7 +144,9 @@ resource "github_branch_protection" "repositories" {
   }
 
   restrict_pushes {
-    push_allowances = try(each.value.is_part_of_monorepo, false) ? [data.github_user.mage-os-ci.node_id] : []
+    push_allowances = try(each.value.is_part_of_monorepo, false)
+      ? [data.github_user.mage-os-ci.node_id]
+      : github_repository.repositories[each.key].teams
   }
 }
 
